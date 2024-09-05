6ce2a66b555bdc6e0d1910ef741452b74a57b3a0
CaioCesarFaria <321caioster@gmail.com>
2024-09-05 13:55:02 -0300

    Layout final provisório

---
+++ script.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay:   
+     3000, // 3000 milliseconds = 3 seconds
+      },
+    
+  });
